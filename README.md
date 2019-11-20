d31b0071adba8f7bf624f0c50949ff4a16f77ddc
Maksym Medynskyi <maxwhite@Maksyms-MacBook-Pro.local>
2019-11-20 18:19:37 +0200

    Initial Magento DevBox commit

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# magento2-docker
 Docker Devbox for Magento 2
+
+# Software stack
+- Nginx (stable)
+- php-fpm (7.2)
+- MySql (percona 5.7)
+- ElasticSearch (6.*)
+- Redis (5.*)
+- Varnish (4.*)
+
+# Usage
+- Install docker and docker compose using [official guidelines](https://docs.docker.com/install/)
+- Checkout this repository
+- Create `.env` file.  All available variables see below
+
+# Stateless Magento build (demo build)
+- Create docker volume `docker volume create --name=magento2-root`
+- Execute `docker-compose up --build` cli command from repository root. For initial start use an env variable `PERFORM_INSTALLATION=1 docker-compose up --build`
+
+
+# Statefull Magento build (devbox)
+- Install `docker-sync` using [official docs](https://docker-sync.readthedocs.io/en/latest/getting-started/installation.html)
+- Configure `docker-sync.yaml` file
+- Start file sync by cli command `docker-sync start -f` from repository root. (For Magento Open Source 2.3.3 codebase time to sync took 6 minutes)
+- Execute `docker-compose up --build` cli command from repository root. Don't use with `PERFORM_INSTALLATION` env variable. All Magento code will be deleted
+
+# Playing with Magento CLI
+- Deploy Magento Sample Data:
+- - `docker exec -it magento2-docker_php7-fpm_1 bin/magento sampledata:deploy` (Composer auth keys required)
+- - `docker exec -it magento2-docker_php7-fpm_1 bin/magento setup:upgrade`
+- - `docker exec -it magento2-docker_php7-fpm_1 bin/magento indexer:reindex`
+- - `docker exec -it magento2-docker_php7-fpm_1 bin/magento c:f config full_page`
+
+- Full Magento reinstall (DB will be cleaned):
+- - `docker exec -it magento2-docker_php7-fpm_1 rm -rf ./var/installed`
+- - `docker exec -it magento2-docker_php7-fpm_1 /docker/scripts/install.sh`
+
+# List of ENV variables
+| VARIABLE | IS_REQUIRED | DEFAULT_VALUE |
+| --- | --- | --- |
+| COMPOSER_MAGENTO_USERNAME | yes | n/a |
+| COMPOSER_MAGENTO_PASSWORD | yes | n/a |
+| MAGENTO_HOST | no | magento2.loc |
+| MAGENTO_ROOT | no | /var/www/magento2 |
+| MAGENTO_MODE | no | developer |
+| MAGE_COMPOSER_STABILITY | no | stable |
+| MYSQL_DATABASE | no | magento2 |
+| MYSQL_USER | no | dbuser |
+| MYSQL_PASSWORD | no | admin |
+| MYSQL_ROOT_PASSWORD | no | root |
+
+Magento will be deployed on `magento2.loc` hostname, so you need to have it in your host's `/etc/hosts` file. Admin panel will be located on `http://magento2.loc/admin` URL. Login: `admin`, password: `adminqwe123`
---
+++ build/php7-fpm/scripts/install.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+magento_admin_username="admin"
+magento_admin_password="adminqwe123"
+magento_admin_email="admin@admin.com"
+magento_admin_firstname="Admin"
+magento_admin_lastname="User"
+magento_baseurl=$APPLICATION_PROTOCOL$APPLICATION_DOMAIN"/"
+
+db_name=$MYSQL_DATABASE
+db_user=$MYSQL_USER
+db_password=$MYSQL_PASSWORD
+
+if [ ! -z $MAGENTO_ADMIN_USERNAME ];
+then
+	magento_admin_username=$MAGENTO_ADMIN_USERNAME
+fi
+
+if [ ! -z $MAGENTO_ADMIN_PASSWORD ];
+then
+	magento_admin_password=$MAGENTO_ADMIN_PASSWORD
+fi
+
+if [ ! -z $MAGENTO_ADMIN_EMAIL ];
+then
+	magento_admin_email=$MAGENTO_ADMIN_EMAIL
+fi
+
+if [ ! -z $MAGENTO_ADMIN_FIRSTNAME ];
+then
+	magento_admin_firstname=$MAGENTO_ADMIN_FIRSTNAME
+fi
+
+if [ ! -z $MAGENTO_ADMIN_LASTNAME ];
+then
+	magento_admin_lastname=$MAGENTO_ADMIN_LASTNAME
+fi
+
+echo "Clean Magento dirs"
+rm -rf ${MAGENTO_ROOT}/generated/*
+rm -rf ${MAGENTO_ROOT}/pub/static/*
+rm -rf ${MAGENTO_ROOT}/var/cache/*
+rm -rf ${MAGENTO_ROOT}/app/etc/config.php
+rm -rf ${MAGENTO_ROOT}/app/etc/env.php
+
+echo "Run Magento installation"
+php ${MAGENTO_ROOT}/bin/magento setup:install --admin-user=$magento_admin_username --admin-password=$magento_admin_password \
+    --admin-email=$magento_admin_email --admin-firstname=$magento_admin_firstname \
+    --admin-lastname=$magento_admin_lastname --db-host=mysql --db-user=$db_user \
+    --db-name=$db_name --db-password=$db_password --base-url=$magento_baseurl \
+    --backend-frontname=admin \
+    --cache-backend=redis --cache-backend-redis-server=redis_cache --cache-backend-redis-db=0 \
+    --page-cache=redis --page-cache-redis-server=redis_cache --page-cache-redis-db=1 --page-cache-redis-compress-data=1 \
+    --session-save=redis --session-save-redis-host=redis_cache --session-save-redis-log-level=3 --session-save-redis-db=2 \
+    --cleanup-database --magento-init-params="MAGE_MODE=${MAGENTO_MODE}"
+ 
+#echo "Set Developer mode"
+#php ${MAGENTO_ROOT}/bin/magento deploy:mode:set ${MAGENTO_MODE}
+
+echo "Set Caching Application to Varnish"
+php ${MAGENTO_ROOT}/bin/magento config:set --scope=default --scope-code=0 system/full_page_cache/caching_application 2
+
+echo "Set Admin Session Lifetime"
+php ${MAGENTO_ROOT}/bin/magento config:set admin/security/session_lifetime 31536000
+
+echo "Set Catalog Search to ElasticSearch"
+php ${MAGENTO_ROOT}/bin/magento config:set catalog/search/engine elasticsearch6
+php ${MAGENTO_ROOT}/bin/magento config:set catalog/search/elasticsearch6_server_hostname elasticsearch
+php ${MAGENTO_ROOT}/bin/magento config:set catalog/search/elasticsearch6_server_port 9200
+php ${MAGENTO_ROOT}/bin/magento config:set catalog/search/elasticsearch6_index_prefix magento2
+
+echo "Start Reindex"
+php ${MAGENTO_ROOT}/bin/magento indexer:reindex
